552de374c0fbda978bd3f70db78eeb500b00d35f
vudinhduong <duong2lophot@gmail.com>
2023-12-02 10:54:31 +0700

    feat: handle access_token

--- src/apis/base-query.ts
+++ src/apis/base-query.ts
@@ -22,7 +22,7 @@ instance.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
-axios.interceptors.response.use(function (response) {
+instance.interceptors.response.use(function (response) {
     return response;
   }, function (error) {
     return Promise.reject(error);
